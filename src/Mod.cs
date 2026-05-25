a8bc695b0712978466c82e6aa9e0e7569108842d
River Mochi <97197236+River-Mochi@users.noreply.github.com>
2026-05-24 23:17:11 -0700

    add polish PL

--- src/Mod.cs
+++ src/Mod.cs
@@ -64,7 +64,7 @@ namespace CityWatchdog
             AddLocaleSource("it-IT", new LocaleIT(setting));
             AddLocaleSource("ja-JP", new LocaleJA(setting));
             AddLocaleSource("ko-KR", new LocaleKO(setting));
-            // AddLocaleSource("pl-PL", new LocalePL(setting));
+            AddLocaleSource("pl-PL", new LocalePL(setting));
             AddLocaleSource("pt-BR", new LocalePT_BR(setting));
             AddLocaleSource("zh-HANS", new LocaleZH_HANS(setting));
             AddLocaleSource("zh-HANT", new LocaleZH_HANT(setting));
